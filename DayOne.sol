249c256d31319f8f31ee8a106dd38dbf8a15b783
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2020-09-10 12:34:07 -0400

    All Assignments Completed
    -Mapping stores people, created new array to store them.
    -When new person created, added to the array.
    -Public function to retrive person based on index.
    -Added address creator to Person struct.
    
    -Added mapping to store count created by specific address.
    -Set/Updated for every new person created.
    
    -Function that returns array of IDs msg.sender created.

--- DayOne.sol
+++ DayOne.sol
@@ -14,13 +14,20 @@ contract HelloWorld{
 
     mapping(address => uint) public creatorCount;
 
+    mapping(address => uint[]) private idArray;
+
     function createPerson(uint id, string memory name, uint age, uint height, address creator) public returns(uint){
         creator = msg.sender;
         people.push(Person(id, name, age, height, creator));
         creatorCount[msg.sender] ++;
+        idArray[msg.sender].push(id);
     }
 
     function getPerson(uint index) public view returns(uint id, string memory name, uint age, uint height, address creator){
         return (people[index].id, people[index].name, people[index].age, people[index].height, people[index].creator);
     }
+
+   function getIDArray() public view returns(uint[] memory){
+       return (idArray[msg.sender]);
+   }
 }
