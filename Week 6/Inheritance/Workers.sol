33c737f2193b1c2f80cdd4fb7834461c34301da1
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2020-09-30 13:01:07 -0400

    Workers With Bosses
    yaFired func restricted to assigned boss

--- Week 6/Inheritance/Workers.sol
+++ Week 6/Inheritance/Workers.sol
@@ -3,54 +3,30 @@ pragma solidity 0.5.12;
 
 contract Workers is People{
 
-    struct Worker{
-        string name;
-        uint age;
-        uint height;
-        uint salary;
-        bool senior;
-        address myBoss;
-    }
+    mapping(address => uint) private salary;
+    mapping(address => address) private boss;
 
-    //mapping(address => uint) private Salary;
-    mapping(address => Worker) private workers;
-
-    function createWorker(string memory name, uint age, uint height, uint salary, address myBoss) public{
-        require(age <= 75, "Worker must be 75 or younger!");
-        require(myBoss != msg.sender);
-        Worker memory newWorker;
-        newWorker.name = name;
-        newWorker.age = age;
-        newWorker.height = height;
-        newWorker.salary = salary;
-        newWorker.myBoss = myBoss;
-
-         if (age >= 65){
-            newWorker.senior = true;
-        } else {
-            newWorker.senior = false;
-        }
-
-        insertWorker(newWorker);
-
-        return createPerson(name, age, height);
-    }
-
-    function insertWorker(Worker memory newWorker) private{
-        workers[msg.sender] = newWorker;
+    function createWorker(string memory _name, uint _age, uint _height, uint _salary, address _myBoss) public{
+        require(_age <= 75, "Worker must be 75 or younger!");
+        require(_myBoss != msg.sender);
+        createPerson(_name, _age, _height);
+        salary[msg.sender] = _salary;
+        boss[msg.sender] = _myBoss;
     }
 
     function getBoss() public view returns(address){
-        return (workers[msg.sender].myBoss);
+        return (boss[msg.sender]);
     }
 
-    function getSalary() public view returns(uint salary){
-       return (workers[msg.sender].salary);
+    function getSalary() public view returns(uint _salary){
+      return (salary[msg.sender]);
     }
 
-    function yaFired(address worker) public{
-        require(msg.sender == workers[worker].myBoss);
-        delete workers[worker];
-        return deletePerson(worker);
+    function yaFired(address _worker) public {
+        require(msg.sender == boss[_worker]);
+        owner = boss[_worker]; //doesn't doing this still reduce code duplication and is cheaper on gas than adding another function? Or is this cheating the assignment?
+        delete salary[_worker];
+        delete boss[_worker];
+        deletePerson(_worker);
     }
 }
