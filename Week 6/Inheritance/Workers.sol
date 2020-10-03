887005c06e07b6d0b3842ab61db23f28d7956954
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2020-10-03 19:35:56 -0400

    Workers with bosses

--- Week 6/Inheritance/Workers.sol
+++ Week 6/Inheritance/Workers.sol
@@ -6,27 +6,26 @@ contract Workers is People{
     mapping(address => uint) private salary;
     mapping(address => address) private boss;
 
-    function createWorker(string memory _name, uint _age, uint _height, uint _salary, address _myBoss) public{
-        require(_age <= 75, "Worker must be 75 or younger!");
-        require(_myBoss != msg.sender);
-        createPerson(_name, _age, _height);
+    function createWorker(string memory name, uint age, uint height, uint _salary, address myBoss) public{
+        require(age <= 75, "Worker must be 75 or younger!");
+        require(myBoss != msg.sender, "You can not be your own boss.");
+        createPerson(name, age, height);
         salary[msg.sender] = _salary;
-        boss[msg.sender] = _myBoss;
+        boss[msg.sender] = myBoss;
     }
 
     function getBoss() public view returns(address){
-        return (boss[msg.sender]);
+        return boss[msg.sender];
     }
 
-    function getSalary() public view returns(uint _salary){
-      return (salary[msg.sender]);
+    function getSalary() public view returns(uint){
+      return salary[msg.sender];
     }
 
-    function yaFired(address _worker) public {
-        require(msg.sender == boss[_worker]);
-        owner = boss[_worker]; //doesn't doing this still reduce code duplication and is cheaper on gas than adding another function? Or is this cheating the assignment?
-        delete salary[_worker];
-        delete boss[_worker];
-        deletePerson(_worker);
+    function yaFired(address worker) public{
+        require(msg.sender == boss[worker], "Must be the boss of the worker to fire.");
+        deleteHuman(worker);
+        delete salary[worker];
+        delete boss[worker];
     }
 }
