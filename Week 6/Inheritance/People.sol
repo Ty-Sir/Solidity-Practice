6750f63b52d978cb6974efa7ef920407b4036f62
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2020-10-03 12:04:44 -0400

    People Contract for Workers
    emits events

--- Week 6/Inheritance/People.sol
+++ Week 6/Inheritance/People.sol
@@ -8,11 +8,12 @@ contract People is Destroyable{
         uint age;
         uint height;
         bool senior;
+        bool exists;
     }
 
-    event personCreated(string name, bool senior);
+    event personCreated(string name, uint age, uint height, bool senior);
     event personDeleted(string name, bool senior, address deletedBy);
-    event personUpdated(string name, uint age, uint height, bool senior);
+    event personUpdated(string name, uint age, uint height, bool senior, string prevName, uint prevAge, uint prevHeight, bool prevSenior);
 
     uint public balance;
 
@@ -27,11 +28,12 @@ contract People is Destroyable{
     function createPerson(string memory name, uint age, uint height) internal{
         require(age < 150, "Age needs to be below 150");
 
-        //balance += msg.value;
+        balance += msg.value;
         Person memory newPerson; //people.push(Person(people.length, name, age, height));
         newPerson.name = name;
         newPerson.age = age;
         newPerson.height = height;
+        newPerson.exists = true;
 
         if (age >= 65){
             newPerson.senior = true;
@@ -39,14 +41,23 @@ contract People is Destroyable{
             newPerson.senior = false;
         }
 
+        string memory prevName = people[msg.sender].name;
+        uint prevAge = people[msg.sender].age;
+        uint prevHeight = people[msg.sender].height;
+        bool prevSenior = people[msg.sender].senior;
+        bool prevExists = people[msg.sender].exists;
+
         insertPerson(newPerson);
         creators.push(msg.sender);
 
         //people[msg.sender] == newPerson--below
         assert(keccak256(abi.encodePacked(people[msg.sender].name, people[msg.sender].age, people[msg.sender].height, people[msg.sender].senior)) == keccak256(abi.encodePacked(newPerson.name, newPerson.age, newPerson.height, newPerson.senior)));
 
-        // emit personCreated(newPerson.name, newPerson.senior);
-        emit personUpdated(newPerson.name, newPerson.age, newPerson.height, newPerson.senior);
+          if (prevExists){
+            emit personUpdated(newPerson.name, newPerson.age, newPerson.height, newPerson.senior, prevName, prevAge, prevHeight, prevSenior);
+        } else {
+            emit personCreated(newPerson.name, newPerson.age, newPerson.height, newPerson.senior);
+        }
     }
 
     function insertPerson(Person memory newPerson) private{
